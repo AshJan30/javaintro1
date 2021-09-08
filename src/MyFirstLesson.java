058e787f8477e9ed06be6042dbd3f2c30616b4ed
sofa <pashjan007004@gmail.com>
2021-09-08 21:39:37 +0400

    Pushing project

---
+++ src/MyFirstLesson.java
@@ -0,0 +1,8 @@
+public class MyFirstLesson {
+    public static void main(String[] args) {
+        int firstVariable;
+        firstVariable = 50;
+        System.out.println(firstVariable);
+
+    }
+}
